9df0eaabfc67f72c334915f90d9f2db2972f1bc8
cbarros7 <cj.barros07@gmail.com>
2022-04-12 23:03:33 -0500

    Modifiy config.sh

--- Configuration/config.sh
+++ Configuration/config.sh
@@ -1,16 +1,9 @@
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common # packages which let apt use packages over HTTPS
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - # add the GPG key for the official Docker repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" # add Docker repository to APT sources
-sudo apt install -y docker-ce # install docker
-
-#sudo usermod -aG docker ${USER} # Execute docker coomand without Sudo
-#su - ${USER}
-
 cd home
+
 mkdir -p ~/.docker/cli-plugins/
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose #Install docker Compose
 sudo chmod +x ~/.docker/cli-plugins/docker-compose # permissions so that the docker compose command
+sudo apt  install -y docker-compose
 
 echo "Ya se ha instalado docker compose"
 
@@ -23,9 +16,8 @@ echo "Ya se ha instalado docker"
 cd # Go to home
 
 #Install Java
-sudo apt-get install openjdk-8-jdk
-sudo apt-get install openjdk-8-jre
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y openjdk-8-jre
 #sudo apt install -y defaul-jre
 #sudo apt install -y defaul-jdk
 
@@ -49,13 +41,12 @@ sudo apt-get -y install -y postgresql-10
 wget -qO - https://packages.confluent.io/deb/7.1/archive.key | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/7.1 stable main"
 sudo add-apt-repository "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install confluent-platform
-
+sudo apt-get update && sudo apt-get install -y confluent-platform
+"""
 sudo chmod u+x /home/$USER/data_streaming_nanodegree/startup.sh
 
 cd /home/$USER/data_streaming_nanodegree/
-
 sudo sh /home/$USER/data_streaming_nanodegree/startup.sh 
-
+"""
 echo "Fin script"
 
