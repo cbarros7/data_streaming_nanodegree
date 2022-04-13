cb8315ec6f2de2425fbb65a0ae6fb06bffde0465
cbarros7 <cj.barros07@gmail.com>
2022-04-12 20:32:07 -0500

    Modify script, install confluent platform and postgresql

--- Configuration/config.sh
+++ Configuration/config.sh
@@ -3,27 +3,55 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - # add the GPG key for the official Docker repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" # add Docker repository to APT sources
 sudo apt install -y docker-ce # install docker
-sudo usermod -aG docker ${USER} # Execute docker coomand without Sudo
-su - ${USER}
 
-mkdir -p ~/.docker/cli-plugins/
+#sudo usermod -aG docker ${USER} # Execute docker coomand without Sudo
+#su - ${USER}
+
+cd home
+sudo mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose #Install docker Compose
 chmod +x ~/.docker/cli-plugins/docker-compose # permissions so that the docker compose command
 
+echo "Ya se ha instalado docker compose"
+
 #git clone https://github.com/linuxacademy/content-kafka-deep-dive.git
-cd InstallKafka/
+
+cd  /home/$USER/data_streaming_nanodegree/Configuration/InstallKafka/
 docker compose up -d --build
 
-cd home
+echo "Ya se ha instalado docker"
+cd # Go to home
 
 #Install Java
-sudo apt install -y defaul-jdk
-sudo apt install -y defaul-jre
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-jre
+sudo apt-get install openjdk-8-jdk
+#sudo apt install -y defaul-jre
+#sudo apt install -y defaul-jdk
+
 
 # Install Kafka
 wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz
 tar -xvf kafka_2.13-3.1.0.tgz
 
 # Add kafka to PATH
-export PATH=$PATH:/home/$USER/Configuration/kafka_2.13-3.1.0/bin
-source ~/.bashrc
+#export PATH=$PATH:/home/$USER/data_streaming_nanodegree/Configuration/kafka_2.13-3.1.0/bin
+#export PATH=$PATH:/home/$USER/kafka_2.13-3.1.0/bin
+#source ~/.bashrc
+
+# Install postgresql
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install postgresql-10
+
+#Install Confluent Platform Packages
+wget -qO - https://packages.confluent.io/deb/7.1/archive.key | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/7.1 stable main"
+sudo add-apt-repository "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install confluent-platform
+
+
+sudo sh /home/$USER/data_streaming_nanodegree/startup.sh 
+
+echo "Fin script"
