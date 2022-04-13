d2aad5fde5f0d3eb0f772c660f778a29155a896e
cbarros7 <cj.barros07@gmail.com>
2022-04-12 23:02:53 -0500

    Add File installDocker

---
+++ Configuration/installDocker.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common # packages which let apt use packages over HTTPS
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - # add the GPG key for the official Docker repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" # add Docker repository to APT sources
+sudo apt install -y docker-ce # install docker
+
+sudo usermod -aG docker ${USER} # Execute docker coomand without Sudo
+su - ${USER}
